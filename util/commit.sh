6b58767bbd825d3317a5693e74f5f894c9d75217
Greg Hamerly <greg_hamerly@baylor.edu>
2022-12-15 11:50:37 -0600

    remove util directory (placing outside of any one year's work)

--- util/commit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Run this from within the directory to be committed.
-
-set -e
-
-NAME=$(grep https *.py | head -n 1 | sed 's/.* "//' | sed 's/"//' | tr '[:upper:]' '[:lower:]')
-DAY=$(basename $(pwd))
-git add . && git commit -m "AoC 2022 day $DAY: $NAME"
